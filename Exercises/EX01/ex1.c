a675406c1617672985c0e26b586c0beea9003c91
lauritsriple <laurits.triple@gmail.com>
2016-01-19 12:29:39 +0100

    Wrote the code for the exercise

---
+++ Exercises/EX01/ex1.c
@@ -0,0 +1,29 @@
+#include <pthread.h>
+#include <stdio.h>
+
+int i = 0;
+pthread_t IOThread;
+
+
+void *func_1(){
+	for (int j =0;j<1000000;j++){
+		i++;
+	}
+}
+
+void *func_2(){
+	for (int j =0;j<1000000;j++){
+		i--;
+	}
+}
+
+int main(){
+	printf("Hello World\n");
+	pthread_t thread1;
+	pthread_t thread2;
+	pthread_create(&thread1,NULL,func_1,NULL);
+	pthread_create(&thread2,NULL,func_2,NULL);
+	printf("The number i is: %i",i);
+	return 0;
+}
+
