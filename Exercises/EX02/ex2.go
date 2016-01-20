8ae22a982ac557e8712fee34158f6d433a280a51
stianlaa <stian.laate@gmail.com>
2016-01-20 14:14:52 +0100

    Finished ex2

--- Exercises/EX02/ex2.go
+++ Exercises/EX02/ex2.go
@@ -5,23 +5,36 @@ import (
 	"runtime"
 )
 
-var i int = 0
-
-func func1() {
+func func1(queue chan int, finish chan int) {
+	temp:=0
 	for j := 0; j < 1000000; j++ {
-		i += 1
+		temp= <- queue
+		temp ++
+		queue <- temp
 	}
+	finish <- 1
 }
-func func2() {
+
+func func2(queue chan int, finish chan int) {
+	temp:=0
 	for j := 0; j < 1000000; j++ {
-		i -= 1
+		temp= <- queue
+		temp --
+		queue <- temp
 	}
+	finish <- 1
 }
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	go func1()
-	go func2()
+	queue := make(chan int,1)
+	finish := make(chan int,2)
+	queue <- 0
+	go func1 (queue,finish)
+	go func2 (queue,finish)
+
+	<- finish
+	<- finish
 	Println("Hello from main")
-	Println(i)
+	Println(<-queue)
 }
