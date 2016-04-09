111c71f5adf028c7a8c973781170011a3d40f1e8
lauritsriple <laurits.triple@gmail.com>
2016-04-09 17:01:14 +0200

    Fixed udp module. Now we only use broadcast because that is easy. Needs ack implementation

--- heis/src/udp/udp.go
+++ heis/src/udp/udp.go
@@ -1,9 +1,11 @@
 package udp
 
 import (
+	"encoding/json"
 	"fmt"
 	"net"
-	"os"	
+	"os"
+	"time"
 )
 
 //How to make libary
@@ -14,47 +16,116 @@ import (
 const PORT string = "20012"
 const BROADCASTADDR string = "129.241.187.255"
 
+const (
+	New Orderstatus = iota
+	Accepted
+	Done
+	Reassign
+)
+
+type Message struct {
+	LiftId    int
+	ReassId   int
+	Floor     int
+	Direction bool
+	Status    Orderstatus
+	Weight    int
+	TimeRecv  time.Time
+}
+
 //Checks the error message and kills the program
 func checkError(err error) {
 	if err != nil {
-		fmt.Println("Error: " , err)
+		fmt.Println("Error: ", err)
 		os.Exit(0)
-	}	
-}
-	
-func Listen(msgCh chan<- string ) { //Writing to chan, ch should be buffered
-	serverAddr,err := net.ResolveUDPAddr("udp",":"+PORT)
-	checkError(err)
-	
-	serverConn, err := net.ListenUDP("udp",serverAddr)
-	checkError(err)
-	
-	defer serverConn.Close() //Closes connection when program stops
-
-	buf:= make([]byte,1024)
-	for {
-		n,_,err := serverConn.ReadFromUDP(buf) //blocking
-		checkError(err)
-
-		msgCh <- string(buf[0:n]) //Sends the message to the buffer
-	}
-	 
-}
-
-func Broadcast(msgCh <-chan string){ //reading from chan
-	ServerAddr,err := net.ResolveUDPAddr("udp",BROADCASTADDR+":"+PORT)
-	checkError(err)
- 
-	Conn, err := net.DialUDP("udp", nil, ServerAddr)
-	checkError(err)
- 
-	defer Conn.Close() //Closes connection when program stops
-	for {
-		buf := []byte(<-msgCh) //Blocking, reads from channel
-		_,err := Conn.Write(buf)
-		checkError(err)
-		//time.Sleep(time.Second * 1)
 	}
 }
 
+//Called by NetInit
+//Returns IPv4 address for lift
+func findIP() (string, *net.Interface, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", nil, err
+	}
+	for _, iface := range ifaces {
+		addrs, _ := iface.Addrs()
+		for _, a := range addrs {
+			if strings.Contains(a.String, "129.") {
+				return a.String(), &iface, nil
+			}
+		}
+	}
+	return "", nil, errors.New("Unable to find IPv4 adress")
+}
 
+//Called by NetInit
+//Returns 3 last digits from IPv4 adress
+func FindID(a string) int {
+	fmt.Println(a)
+	id, err := strconv.Atoi(strings.Split(a, ".")[3][:3])
+	if err != nil {
+		fmt.Println("Error converting IP to ID", err)
+	}
+	return id
+}
+
+//Sets up the broadcast
+//Called by NetInit
+func BroadcastInit(send <-chan Message, recv chan<- Message, iface *net.Interface, quit <-chan bool) {
+	group, err := net.ResolveUDPAddr("udp", BROADCASTADDR)
+	checkError(err)
+	conn, err := net.ListenMulticastUDP("udp", iface, group)
+	checkError(err)
+	defer conn.Close()
+	go broadcastListen(recv, conn)
+	go broadcastSend(send, conn, group)
+	fmt.Println("Network running")
+	<-quit //w8 for channel to be true. Defer will be called
+}
+
+//Workerthread. Called by BroadcastInit
+func broadcastListen(send <-chan Message, conn *net.UDPConn, addr *net.UDPAddr) {
+	for {
+		select {
+		case m := <-send:
+			buf, err := json.Marshal(m)
+			if err != nil {
+				fmt.Println("JSON encoding: ", err)
+			} else {
+				_, err := conn.WriteToUDP(buf, addr)
+				if err != nil {
+					fmt.Println("NET: ", err)
+				}
+			}
+		case <-quit:
+			return
+		}
+	}
+}
+
+//Workerthread. Called by BroadcastInit
+func broadcastSend(recv chan<- Message, conn *net.UDPConn) {
+	for {
+		buf := make([]byte, 512)
+		l, _, err := conn.ReadFrom(buf)
+		if err != nil {
+			fmt.Println("NET: ", err)
+		} else {
+			m.Timerecv = time.Now()
+			recv <- m
+		}
+	}
+}
+
+//Called by
+//Sets up the network and returns the id of the elevator
+func NetInit(send <-chan Message, recv chan<- Message, quitch *chan bool) int {
+	addr, iface, err := FindIP()
+	if err != nil {
+		fmt.Println("Error findig the interface", err)
+		return 0
+	}
+	go BroadcastInit(send, recv, iface, quitch)
+	return FindID(addr)
+}
